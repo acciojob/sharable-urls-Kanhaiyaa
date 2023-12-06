8c8f4d0efb2ea220a8b249858605d4d844858765
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-06 06:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+function generateURL() {
+            // Get values from the input fields
+            var name = document.getElementById("name").value;
+            var year = document.getElementById("year").value;
+
+	var quaryString="?name="+encodeURIComponent(name)+"&year="+encodeURIComponent(year)
+
+	document.getElementById("url").textContent="https://localhost:8080/"+ quaryString
+}
